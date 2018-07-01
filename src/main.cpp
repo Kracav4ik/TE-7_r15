051dc7fb0b04439ae104b1c9d28e39367d52747c
Kracav4ik <dikama2013@yandex.ru>
2018-07-01 19:50:46 +0300

    SDL2 works!

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
+#include <SDL2/SDL.h>
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    auto window = SDL_CreateWindow("SDL2 Window", 100, 100, 640, 480, 0);
+
+    if (!window) {
+        std::cerr << "Could not create window: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return 0;
 }
