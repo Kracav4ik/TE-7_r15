96aeeb46963597d9e148bb8037818c9e35ca7022
Kracav4ik <dikama2013@yandex.ru>
2018-07-02 05:31:55 +0300

    separate game loop

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,9 @@ const int SCREEN_WIDTH = 650;
 const int SCREEN_HEIGHT = 500;
 const char* const NAME = "SDL2 Window";
 
-void render(Block& block, SDL_Surface* screenSurface);
+bool handleInput(Block& active_block);
+void process(Block& activeBlock);
+void render(Block& block, SDL_Surface* screenSurface, SDL_Window* window);
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
@@ -19,35 +21,14 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    Block active_block(0, 0, BLOCK_SIZE, BLOCK_SIZE);
+    Block activeBlock(0, 0, BLOCK_SIZE, BLOCK_SIZE);
     bool quit = false;
-    SDL_Event event;
+    auto screenSurface = SDL_GetWindowSurface(window);
     while (!quit) {
-        auto screenSurface = SDL_GetWindowSurface(window);
-        render(active_block, screenSurface);
-        while (SDL_PollEvent(&event) != 0) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            }
-            if (event.type == SDL_KEYDOWN) {
-                switch (event.key.keysym.sym) {
-                    case SDLK_LEFT:
-                        active_block.left(0);
-                        break;
-                    case SDLK_RIGHT:
-                        active_block.right(SCREEN_WIDTH);
-                        break;
-                    case SDLK_DOWN:
-                        active_block.force_shift(SCREEN_HEIGHT);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        SDL_UpdateWindowSurface(window);
+        quit = handleInput(activeBlock);
+        process(activeBlock);
+        render(activeBlock, screenSurface, window);
         SDL_Delay(20);
-        active_block.shift(SCREEN_HEIGHT);
     }
 
     SDL_DestroyWindow(window);
@@ -57,7 +38,38 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-void render(Block& block, SDL_Surface* screenSurface) {
+bool handleInput(Block& active_block) {
+    bool quit = false;
+    SDL_Event event;
+    while (SDL_PollEvent(&event) != 0) {
+        if (event.type == SDL_QUIT) {
+            quit = true;
+        }
+        if (event.type == SDL_KEYDOWN) {
+            switch (event.key.keysym.sym) {
+                case SDLK_LEFT:
+                    active_block.left(0);
+                    break;
+                case SDLK_RIGHT:
+                    active_block.right(SCREEN_WIDTH);
+                    break;
+                case SDLK_DOWN:
+                    active_block.force_shift(SCREEN_HEIGHT);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    return quit;
+}
+
+void process(Block& activeBlock) {
+    activeBlock.shift(SCREEN_HEIGHT);
+}
+
+void render(Block& block, SDL_Surface* screenSurface, SDL_Window* window) {
     SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
     SDL_FillRect(screenSurface, block.getRect(), SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window);
 }
